89c452840d76161a1e1a5150514a9a486efa7396
codrin2 <hssang127@gmail.com>
2023-12-14 11:19:08 +0900

    add redis config

--- src/main/java/com/poppin/userservice/config/RedisConfig.java
+++ src/main/java/com/poppin/userservice/config/RedisConfig.java
@@ -1,43 +1,67 @@
 package com.poppin.userservice.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int port;
 
-    @Value("${spring.redis.port}")
-    private String redisPort;
+    @Value("${spring.data.redis.password}")
+    private String password;
 
-    @Value("${spring.redis.password}")
-    private String redisPassword;
-
+    @Value("${spring.data.redis.timeout}")
+    private Long timeout;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisHost);
-        redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
-        redisStandaloneConfiguration.setPassword(redisPassword);
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-        return lettuceConnectionFactory;
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        final SocketOptions socketoptions = SocketOptions.builder().connectTimeout(Duration.ofSeconds(10)).build();
+        final ClientOptions clientoptions = ClientOptions.builder().socketOptions(socketoptions).build();
+        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder().clientOptions(clientoptions)
+                .commandTimeout(Duration.ofMinutes(1))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);  // Set the password
+        redisStandaloneConfiguration.setDatabase(0);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
+    }
+
+    @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }
